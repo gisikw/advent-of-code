c7d6ece6a94f83f82a3619a9345b30d0c1e864af
Kevin Gisi <kevin.gisi@calendly.com>
2023-11-25 13:54:41 -0600

    Add validation to set.sh

--- lib/set.sh
+++ lib/set.sh
@@ -4,8 +4,28 @@ _usage() {
 }
 
 main() {
-  _confirm "Set year to $1, day to $2, and language to $3?"
+  # Validate Year
+  if ! [[ $1 =~ ^[0-9]{4}$ ]]; then
+    echo "Error: Year must be a four-digit number."
+    return 1
+  fi
+
+  # Validate Day
+  if ! [[ $2 =~ ^[0-9]+$ ]] || [ $2 -lt 1 ] || [ $2 -gt 25 ]; then
+    echo "Error: Day must be an integer between 1 and 25."
+    return 1
+  fi
+
+  # Validate Language
+  local valid_langs=$(yq eval '.languages | keys' "${local_path}/config.yml")
+  if ! [[ " $valid_langs " =~ " $3 " ]]; then
+    echo "Error: Language must be one of the supported languages."
+    echo "$valid_langs"
+    return 1
+  fi
+
   echo "export AOC_YEAR=$1" >> $AOC_TEMPFILE
   echo "export AOC_DAY=$2" >> $AOC_TEMPFILE
   echo "export AOC_LANG=$3" >> $AOC_TEMPFILE
 }
+
