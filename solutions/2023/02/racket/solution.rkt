465bec13ffceb549bc38682ff43b53f193d5eb61
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-02 09:35:17 -0600

    Revised AoC 2023/2/racket to align more with idomatic LISP

--- solutions/2023/02/racket/solution.rkt
+++ solutions/2023/02/racket/solution.rkt
@@ -9,23 +9,40 @@
           (part2 input-file))))
 
 (define (part1 input-file)
-  (let ([games (for/list ([line (in-lines (open-input-file input-file))]) 
-                    (make-game line))])
+  (let* ([lines (for/list ([line (in-lines (open-input-file input-file))]) line)]
+         [games (map parse-game lines)])
     (printf "~a\n" (for/sum ([id (map car (filter meets-part-1-minimums games))]) id))))
 
-(define (make-game line)
-  (list
-    (string->number (second (regexp-match #px"^Game (\\d+)" line)))
-    (max-color-count line "red")
-    (max-color-count line "green")
-    (max-color-count line "blue")
-  ))
+(define (parse-game line)
+  (match (string-split line ":") 
+    [(list game-string cubes-string)
+     (cons 
+       (parse-game-id game-string) 
+       (parse-revealed-cubes cubes-string))]))
 
-(define (max-color-count line color)
-  (let ([matches (regexp-match* (pregexp (string-append "(\\d+) " color)) line #:match-select values)])
-    (if matches
-      (apply max (map (lambda (match) (string->number (second match))) matches))
-      0)))
+(define (parse-game-id game-string)
+  (string->number (second (string-split game-string))))
+
+(define (parse-revealed-cubes cubes-string)
+  (let* ([pull-strings (string-split cubes-string ";")]
+         [pull-results (apply append (map parse-pull-string pull-strings))])
+    (list
+      (max-of-color pull-results "red")
+      (max-of-color pull-results "green")
+      (max-of-color pull-results "blue"))))
+
+(define (parse-pull-string pull-string)
+  (let ([pair-strings (string-split pull-string ",")])
+    (map parse-pair-string pair-strings)))
+
+(define (parse-pair-string pair-string)
+  (match (string-split pair-string " ")
+    [(list num color)
+     (list (string->number num) color)]))
+
+(define (max-of-color all-pairs color)
+  (let ([pairs (filter (lambda (pair) (string=? (second pair) color)) all-pairs)])
+    (apply max (map first pairs))))
 
 (define (meets-part-1-minimums game)
   (and
@@ -39,7 +56,7 @@
 
 (define (part2 input-file)
   (let ([games (for/list ([line (in-lines (open-input-file input-file))]) 
-                    (make-game line))])
+                    (parse-game line))])
     (printf "~a\n" (for/sum ([power (map game-power games)]) power))))
 
 (define (game-power game)
