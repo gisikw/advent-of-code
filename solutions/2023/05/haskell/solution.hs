ad78a01a34fbc47ffc16b5381f2f4206fc739136
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-05 13:45:08 -0600

    Performance improvements for 2023/05/haskell (14min runtime)

--- solutions/2023/05/haskell/solution.hs
+++ solutions/2023/05/haskell/solution.hs
@@ -1,43 +1,52 @@
 import System.Environment
+import Data.List (sortBy, foldl')
+import Data.Ord (comparing)
+
+data Transformation = Transformation { lb :: Int, ub :: Int, delta :: Int } deriving (Show)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let (inputFile:part:_) = args
-  content <- readFile inputFile
-  let (seeds, definitions) = parseContent content
-  let results = case (read part) of
-                  1 -> minimum $ map (processTransforms definitions) seeds 
-                  2 -> seekMinimum seeds definitions maxBound :: Int
-  print results
+  case args of
+    [inputFile, part] -> do
+      content <- readFile inputFile
+      let (seeds, definitions) = parseContent content
+      let results = case (read part) of
+                      1 -> minimum $ map (processTransforms definitions) seeds
+                      2 -> seekMinimum seeds definitions maxBound :: Int
+      print results
+    _ -> putStrLn "Incorrect arguments"
 
 parseContent content =
-  let input = lines content
-  in (parseSeeds $ head input, parseDefinitions input)
+  case lines content of
+    (seedLine : defLines) -> (parseSeeds seedLine, parseDefinitions defLines)
+    _ -> error "Malformed input file"
 
 parseSeeds line = map (read :: String -> Int) (drop 1 $ words line)
 
 parseDefinitions input = drop 1 $ reverse $ map parseBlock $ splitBlocks input [] []
 
-splitBlocks [] current defs = (current:defs)
-splitBlocks ("":ls) current defs = splitBlocks ls [] (current:defs)
-splitBlocks (l:ls) current defs = splitBlocks ls (current ++ [l]) defs
+splitBlocks [] current defs = reverse current : defs
+splitBlocks ("" : ls) current defs = splitBlocks ls [] (reverse current : defs)
+splitBlocks (l : ls) current defs = splitBlocks ls (l : current) defs
 
-parseBlock block = map parseRange $ drop 1 block
+parseBlock block = sortBy (comparing lb) $ map parseRange $ drop 1 block
 
-parseRange line = map (read :: String -> Int) $ words line
+parseRange line =
+  let [dst, src, len] = map (read :: String -> Int) $ words line
+  in Transformation src (src + len) (dst - src)
 
+processTransforms definitions seed = foldl' transform seed definitions
+
+transform :: Int -> [Transformation] -> Int
 transform n [] = n
-transform n (r:rs)
-  | delta >= 0 && delta < len = dst + delta
+transform n (Transformation lb ub change : rs)
+  | n >= lb && n < ub = n + change
+  | n < lb = n
   | otherwise = transform n rs
-  where 
-    dst = r !! 0
-    delta = n - (r !! 1)
-    len = r !! 2
-
-processTransforms definitions seed = foldl transform seed definitions
 
 seekMinimum [] defs min = min
-seekMinimum (a:1:rest) defs min = seekMinimum rest defs min
-seekMinimum (a:b:rest) defs min = seekMinimum (succ(a):pred(b):rest) defs (minimum [min, (processTransforms defs a)])
+seekMinimum (a : 1 : rest) defs min = seekMinimum rest defs min
+seekMinimum (a : b : rest) defs min =
+  let nextMin = minimum [min, (processTransforms defs a)]
+  in seekMinimum (succ(a) : pred(b) : rest) defs nextMin
