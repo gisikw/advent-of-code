6b116288e8a84fd798351aac29681925206cc3f5
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-07 08:31:32 -0600

    Cleanup

--- solutions/2023/07/node/solution.js
+++ solutions/2023/07/node/solution.js
@@ -7,18 +7,8 @@ const main = (inputFile, part) => {
 
   const hands = lines.map(line => {
     const [str, bet] = line.split(' ');
-    let rankable = str
-      .replaceAll('T','V')
-      .replaceAll('J','W')
-      .replaceAll('Q','X')
-      .replaceAll('K','Y')
-      .replaceAll('A','Z')
-    if (part == "2") rankable = rankable.replaceAll('W', '1');
-    const groups = rankable
-      .split('')
-      .sort()
-      .join('')
-      .matchAll(/(.)\1*/g);
+    let sortableHand = renameFaceCards(str, part);
+    const groups = sortableHand.split('').sort().join('').matchAll(/(.)\1*/g);
     let hand = [...groups].map(g => g[0]).sort(pokerSort).join('');
 
     if (part == "2") {
@@ -27,22 +17,22 @@ const main = (inputFile, part) => {
       hand = `${(hand[0]||'Z').repeat(jokers)}${hand}`;
     }
 
-    return [
-      rankable,
-      hand,
-      parseInt(bet),
-      handKind(hand)
-    ];
+    return [sortableHand, parseInt(bet), handKind(hand)];
   });
 
   hands.sort(pokerHandSort);
 
-  const sum = hands.reduce((acc, [_, __, bet], idx) => {
-    return acc + ((idx + 1) * bet)
-  }, 0);
-
+  const sum = hands.reduce((acc, [_, bet], idx) => acc + (idx + 1) * bet, 0);
   console.log(sum);
 };
+
+const renameFaceCards = (str, part) =>
+    str
+      .replaceAll('T','V')
+      .replaceAll('J',part == "1" ? 'W' : '1')
+      .replaceAll('Q','X')
+      .replaceAll('K','Y')
+      .replaceAll('A','Z');
 
 const pokerSort = (a, b) => {
   const lenCpr = b.length - a.length;
@@ -51,19 +41,19 @@ const pokerSort = (a, b) => {
 }
 
 const pokerHandSort = (a, b) => {
-  handTypeCpr = a[3] - b[3];
+  handTypeCpr = a[2] - b[2];
   if (handTypeCpr != 0) return handTypeCpr;
   return a[0] < b[0] ? -1 : 1;
 }
 
 const handKind = (hand) => {
-  if (hand.match(/(.)\1{4}/)) return 7; // Five of a kind
-  if (hand.match(/(.)\1{3}/)) return 6; // Four of a kind
-  if (hand.match(/(.)\1{2}(.)\2/)) return 5; // Full house
-  if (hand.match(/(.)\1{2}/)) return 4; // Three of a kind
-  if (hand.match(/(.)\1(.)\2/)) return 3; // Two pair
-  if (hand.match(/(.)\1/)) return 2; // Pair
-  return 1; // High Card
+  if (hand.match(/(.)\1{4}/)) return 7;
+  if (hand.match(/(.)\1{3}/)) return 6;
+  if (hand.match(/(.)\1{2}(.)\2/)) return 5;
+  if (hand.match(/(.)\1{2}/)) return 4;
+  if (hand.match(/(.)\1(.)\2/)) return 3;
+  if (hand.match(/(.)\1/)) return 2;
+  return 1;
 }
 
 const inputFile = process.argv[2];
