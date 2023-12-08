7b4e0fb2565863e79e61c3ba680eb2054faf93f1
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-08 12:33:51 -0600

    Revise 2023/08/erlang for correctness when loop % path != 0

--- solutions/2023/08/erlang/solution.erl
+++ solutions/2023/08/erlang/solution.erl
@@ -6,44 +6,41 @@ main([InputFile, Part]) ->
     Content = binary_to_list(BinaryContent),
     [ Path | [ _ | NodeLines ] ] = string:split(rtrim(Content), "\n", all),
     Route = parse_path(Path),
-    Nodes = make_nodes(NodeLines, dict:new()),
+    Nodes = make_nodes(NodeLines),
     Result = case Part of 
-               "1" -> traverse("AAA", 0, Route, Nodes, fun is_zzz/1);
+               "1" -> traverse("AAA", Route, Nodes, fun is_zzz/1);
                  _ -> GhostNodes = find_ghost_nodes(Nodes),
                       Loops = sort_descending(get_loop_lengths(GhostNodes, Route, Nodes)),
-                      % Laziest LCM
-                      Product = lists:foldl(fun (A, B) -> A * B end, 1, Loops),
+                      Product = lists:foldl(fun (A, B) -> A * B end, 1, Loops), % Laziest LCM
                       Product * length(Route)
              end,
     io:format("~p~n", [Result]).
 
+% Lazy assumption: len(A->Z) == len(Z->Z)
 get_loop_lengths(StartingNodes, Route, Nodes) ->
-  lists:map(
-    fun (N) -> trunc(traverse(N, 0, Route, Nodes, fun ends_in_z/1) / length(Route)) end,
-    StartingNodes).
+  [trunc(traverse(N, Route, Nodes, fun ends_in_z/1) / length(Route)) || N <- StartingNodes].
 
-% 76 being ASCII "L"
 parse_path(String) -> 
-  lists:map(fun(C) -> case C of 76 -> 1; _ -> 2 end end, String).
+  [if C == $L -> 1; true -> 2 end || C <- String].
 
-traverse(CurrentNode, Distance, [ Direction | Rest ], Nodes, ArrivedFn) ->
-  case ArrivedFn(CurrentNode) of
+traverse(Node, Route, Nodes, Pred) -> traverse(Node, Route, Nodes, Pred, 0, Route).
+traverse(Node, Route, Nodes, Pred, Distance, InitialRoute) ->
+  case Pred(Node) andalso Route == InitialRoute of
     true -> Distance;
-    _ -> NextNode = element(Direction, dict:fetch(CurrentNode, Nodes)),
+    _ -> [ Direction | Rest ] = Route,
+         NextNode = element(Direction, maps:get(Node, Nodes)),
          NextRoute = lists:append(Rest, [Direction]),
-         traverse(NextNode, Distance + 1, NextRoute, Nodes, ArrivedFn)
+         traverse(NextNode, NextRoute, Nodes, Pred, Distance + 1, InitialRoute)
   end.
 
-make_nodes([], Dict) -> Dict;
-make_nodes([ Head | Tail ], Dict) ->
+make_nodes(Lines) -> make_nodes(Lines, maps:new()).
+make_nodes([], Map) -> Map;
+make_nodes([ Head | Tail ], Map) ->
   [Key, Left, Right] = string:lexemes(Head, " =,()"),
-  make_nodes(Tail, dict:store(Key, {Left, Right}, Dict)).
+  make_nodes(Tail, Map#{Key => {Left, Right}}).
 
-% 65 being ASCII "A"
 find_ghost_nodes(Nodes) ->
-  lists:filtermap(
-    fun(C) -> case ends_in_a(C) of true -> {true,C}; _ -> false end end,
-    dict:fetch_keys(Nodes)).
+    [C || C <- maps:keys(Nodes), ends_in_a(C)].
 
 rtrim(String) ->
     case lists:reverse(String) of
@@ -56,8 +53,8 @@ sort_descending(List) -> lists:reverse(lists:sort(List)).
 is_zzz("ZZZ") -> true;
 is_zzz(_) -> false.
 
-ends_in_a([_,_,65]) -> true;
+ends_in_a([_,_,$A]) -> true;
 ends_in_a(_) -> false.
 
-ends_in_z([_,_,90]) -> true;
+ends_in_z([_,_,$Z]) -> true;
 ends_in_z(_) -> false.
