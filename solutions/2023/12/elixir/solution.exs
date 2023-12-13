263a8ac2e61117e362fd4c141de9cf7773686bf9
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-12 22:45:47 -0600

    Cleanup

--- solutions/2023/12/elixir/solution.exs
+++ solutions/2023/12/elixir/solution.exs
@@ -1,72 +1,112 @@
 defmodule Solution do
   def main(args) do
     [input_file, part] = args
+
     File.read!(input_file)
-      |> String.split("\n", trim: true)
-      |> Task.async_stream(&variations(part,&1), ordered: false, max_concurrency: 100, timeout: :infinity)
-      |> Enum.reduce(0, fn { :ok, n }, acc -> acc + n end)
-      |> IO.puts
+    |> String.split("\n", trim: true)
+    |> Task.async_stream(&variations(part, &1),
+      ordered: false,
+      max_concurrency: 8,
+      timeout: :infinity
+    )
+    |> Enum.reduce(0, fn {:ok, n}, acc -> acc + n end)
+    |> IO.puts()
   end
 
   def variations(part, line) do
-    [springs, sums] = String.split(line, " ") 
-    springs = if part == "1", do: springs, else: springs |> List.duplicate(5) |> Enum.join("?")
-    sums = if part == "1", do: sums, else: sums |> List.duplicate(5) |> Enum.join(",")
-    { res, _ } = match(
-      springs |> String.replace(~r/\.{2,}/,".") |> String.replace_leading(".", "") |> String.split("", trim: true),
-      sums |> String.split(",") |> Enum.map(fn i -> String.to_integer(i) end)
-    )
+    {springs, sums} = parse_line(part, line)
+    {res, _} = match(prepare_springs(springs), prepare_sums(sums))
     res
   end
 
+  def parse_line("1", line) do
+    [springs, sums] = String.split(line, " ")
+    {springs, sums}
+  end
+
+  def parse_line("2", line) do
+    [springs, sums] = String.split(line, " ")
+    {springs |> List.duplicate(5) |> Enum.join("?"), sums |> List.duplicate(5) |> Enum.join(",")}
+  end
+
+  def prepare_springs(springs) do
+    springs
+    |> String.replace(~r/\.{2,}/, ".")
+    |> String.replace_leading(".", "")
+    |> String.split("", trim: true)
+  end
+
+  def prepare_sums(sums) do
+    sums
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
   def match(spr, seqs, cache \\ %{})
+
   def match(spr, [], cache) do
     score = if "#" in spr, do: 0, else: 1
-    { score, cache |> Map.put({spr,[]}, score) }
+    {score, Map.put(cache, {spr, []}, score)}
   end
-  def match([], seqs, cache), do: { 0, cache |> Map.put({[],seqs}, 0) }
+
+  def match([], seqs, cache) do
+    {0, Map.put(cache, {[], seqs}, 0)}
+  end
+
+  def match(spr, seqs, cache) when is_map_key(cache, {spr, seqs}) do
+    {cache[{spr, seqs}], cache}
+  end
+
   def match(spr, seqs, cache) do
-    case cache[{spr, seqs}] do
-      nil -> 
-        [seq | rest] = seqs
-        case chomp_seq(spr, seq) do
-          { :exact, rem } -> match(rem, rest, cache)
-          { :fuzzy, rem } -> 
-            [ _ | tail ] = spr
-            { score1, cache1 } = match(rem, rest, cache)
-            { score2, cache2 } = match(chomp_spaces(tail), seqs, cache1)
-            score = score1 + score2
-            { score, cache2 |> Map.put({spr,seqs}, score) }
-          { :none, _ } -> 
-            case spr do
-              [ "#" | _ ] -> { 0, cache |> Map.put({spr,seqs},0) }
-              [ _ | tail ] -> match(chomp_spaces(tail), seqs, cache)
-            end
-        end
-      score -> { score, cache }
+    [seq | rest] = seqs
+
+    case chomp_seq(spr, seq) do
+      {:exact, rem} -> match(rem, rest, cache)
+      {:fuzzy, rem} -> process_fuzzy_match(spr, seqs, cache, rem, rest)
+      {:none, _} -> process_no_match(spr, seqs, cache)
     end
   end
 
+  def process_fuzzy_match(spr, seqs, cache, rem, rest) do
+    {score1, cache1} = match(rem, rest, cache)
+    {score2, cache2} = match(chomp_spaces(tl(spr)), seqs, cache1)
+    {score1 + score2, Map.put(cache2, {spr, seqs}, score1 + score2)}
+  end
+
+  def process_no_match(spr, seqs, cache) when hd(spr) == "#" do
+    {0, Map.put(cache, {spr, seqs}, 0)}
+  end
+
+  def process_no_match([_ | tail], seqs, cache) do
+    match(chomp_spaces(tail), seqs, cache)
+  end
+
   def chomp_seq(spr, seq) do
-    input = spr |> chomp_spaces
-    case [input, input |> chomp_maybes(seq)] do
-      [_, nil] -> { :none, nil }
-      [["#"|_], result] -> { :exact, result }
-      [_, result] -> { :fuzzy, result }
+    input = chomp_spaces(spr)
+    chomped = chomp_maybes(input, seq)
+
+    cond do
+      chomped == nil ->
+        {:none, nil}
+
+      hd(input) == "#" ->
+        {:exact, chomped}
+
+      true ->
+        {:fuzzy, chomped}
     end
   end
 
   def chomp_spaces([]), do: []
-  def chomp_spaces(["."|rest]), do: chomp_spaces(rest)
+  def chomp_spaces(["." | rest]), do: chomp_spaces(rest)
   def chomp_spaces(spr), do: spr
 
   def chomp_maybes([], 0), do: []
   def chomp_maybes([], _), do: nil
-  def chomp_maybes(["#"|_], 0), do: nil
-  def chomp_maybes([_|rest], 0), do: chomp_spaces(rest)
-  def chomp_maybes(["."|_],_), do: nil
-  def chomp_maybes([_|rest],n), do: chomp_maybes(rest, n-1)
-
+  def chomp_maybes(["#" | _], 0), do: nil
+  def chomp_maybes([_ | rest], 0), do: chomp_spaces(rest)
+  def chomp_maybes(["." | _], _), do: nil
+  def chomp_maybes([_ | rest], n), do: chomp_maybes(rest, n - 1)
 end
 
 Solution.main(System.argv())
