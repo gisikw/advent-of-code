ac6b06e16e25cc3090ece3bcdf2ef1053d560759
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-23 17:14:00 -0600

    Part 2 of 2023/23/clojure

--- solutions/2023/23/clojure/solution.clj
+++ solutions/2023/23/clojure/solution.clj
@@ -37,15 +37,15 @@
 (defn all-neighbors [grid at prev]
   (filterv (partial is-open grid)
     (filterv (partial in-bounds grid)
-      (filterv #(not (includes? prev %))
+      (filterv #(not= prev %)
         (adjacent-tiles at)))))
 
 (defn neighbors [grid at prev]
   (case (char-at grid at)
-    \^ (filterv #(not (includes? prev %)) [[ (- 1 (first at)) (second at)]])
-    \< (filterv #(not (includes? prev %)) [[ (first at) (- 1 (second at))]])
-    \> (filterv #(not (includes? prev %)) [[ (first at) (+ 1 (second at))]])
-    \v (filterv #(not (includes? prev %)) [[ (+ 1 (first at)) (second at)]])
+    ; \^ (filterv #(not (includes? prev %)) [[ (- 1 (first at)) (second at)]])
+    ; \< (filterv #(not (includes? prev %)) [[ (first at) (- 1 (second at))]])
+    ; \> (filterv #(not (includes? prev %)) [[ (first at) (+ 1 (second at))]])
+    ; \v (filterv #(not (includes? prev %)) [[ (+ 1 (first at)) (second at)]])
     (all-neighbors grid at prev)))
 
 (defn make-next [steps prev coord]
@@ -55,8 +55,11 @@
   (let [coord (first state)
         steps (second state)
         prev (nth state 2)
-        n (neighbors grid coord prev)]
-    (map (partial make-next steps (conj [coord] prev)) n)))
+        n (neighbors grid coord prev)
+        ;_ (println "At " coord " with prev " prev)
+        nextPrev (conj prev coord)]
+        ; nextPrev (conj (if (> (count n) 1) prev (pop prev)) coord)]
+    (map (partial make-next steps nextPrev) n)))
 
 (defn max-dist [grid dest best states]
   (if (empty? states) best
@@ -64,8 +67,42 @@
           coord (first state)
           steps (second state)]
       (if (= coord dest)
-        (recur grid dest (max best steps) (rest states))
+        (let [_ (println "Found " steps " (" best ")" )]
+          (recur grid dest (max best steps) (rest states)))
         (recur grid dest best (concat (make-nexts grid state) (rest states)))))))
+
+(defn find-next-node [grid graph steps [prevRow prevCol] [row col]]
+  (if (contains? graph [row col]) [[row col] steps]
+   (let [n (neighbors grid [row col] [prevRow prevCol])]
+    (if (> (count n) 1) [[row col] steps]
+     (recur grid graph (+ 1 steps) [row col] (first n))))))
+
+(defn build-graph [grid graph [node & remaining]]
+  (if (not node) graph
+    (let [;_ (println "evalling " node)
+          next-nodes (mapv (partial find-next-node grid graph 1 node) (neighbors grid node [-1 -1]))
+          graph (assoc graph node (into (sorted-map) next-nodes))
+          unprocessed (remove #(contains? graph %) (map first next-nodes))
+          ;_ (println "Unprocessed: " unprocessed)
+          ]
+      (recur grid graph (concat remaining unprocessed)))))
+
+(defn make-sub [graph parent steps visited node]
+  [node (+ steps ((graph parent) node)) visited])
+
+(defn subtraversals [graph [node steps visited]] ; -> [[node steps visited]]
+  (let [nexts (remove #(includes? visited %) (keys (graph node)))]
+    (map (partial make-sub graph node steps (conj visited node)) nexts)))
+
+(defn traverse [graph dest best [state & remaining]]
+  (if (not state) best
+    (let [[node steps visited] state]
+      (if (= node dest)
+        (let [
+              ;_ (println "Reached dest in " steps " steps")
+            ]
+          (recur graph dest (max best steps) remaining))
+        (recur graph dest best (concat (subtraversals graph state) remaining))))))
  
 (defn -main [& args]
   (let [input-file (first args)
@@ -73,5 +110,8 @@
         content (slurp input-file)
         grid (str/split content #"\n")
         start (start-pos grid)
-        end (end-pos grid)]
-    (println (max-dist grid end 0 [[start 0 [-1 -1]]]))))
+        end (end-pos grid)
+        graph (build-graph grid { end {} } [start])
+        ]
+    (println (traverse graph end 0 [[start 0 []]]))
+    ))
