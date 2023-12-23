b9c7a262fc30d76da58e54f6b0070ed6532f6c5b
Kevin Gisi <kevin.gisi@calendly.com>
2023-12-23 03:10:47 -0600

    Refactored to use vectors everywhere for proper include-detection

--- solutions/2023/23/clojure/solution.clj
+++ solutions/2023/23/clojure/solution.clj
@@ -4,20 +4,17 @@
 (defn stacksize [] (try (throw (Exception. "")) (catch Exception e (count (.getStackTrace e)))))
 (defn prss [] (print (stacksize) " "))
 
-(defn start-pos [grid] 
-  (list 0 (str/index-of (first grid) ".")))
+(defn start-pos [grid]
+  [0 (str/index-of (first grid) ".")])
 
 (defn end-pos [grid] 
-  (list (- (count grid) 1) (str/index-of (last grid) ".")))
+  [(- (count grid) 1) (str/index-of (last grid) ".")])
 
-(defn char-at [grid coord]
-  (nth (nth grid (first coord)) (second coord)))
+(defn char-at [grid [row col]]
+  (nth (grid row) col))
 
-(defn in-bounds [grid coord]
-  (let 
-    [row (first coord)
-     col (second coord)
-     size (count grid)]
+(defn in-bounds [grid [row col]]
+  (let [size (count grid)]
     (cond
       (< row 0) false
       (< col 0) false
@@ -28,39 +25,38 @@
 (defn is-open [grid coord]
   (not= \# (char-at grid coord)))
 
-(defn adjacent-tiles [coord]
-  (let
-    [row (first coord)
-     col (second coord)]
-    (list 
-      (list (+ row 1) col)
-      (list (- row 1) col)
-      (list row (+ col 1))
-      (list row (- col 1)))))
+(defn adjacent-tiles [[row col]]
+  [[(+ row 1) col]
+   [(- row 1) col]
+   [row (+ col 1)]
+   [row (- col 1)]])
 
-(defn all-neighbors [grid at from]
-  (filter (partial is-open grid)
-    (filter (partial in-bounds grid)
-      (filter #(not= % from) 
+(defn includes? [prev item]
+  (some #{item} prev))
+
+(defn all-neighbors [grid at prev]
+  (filterv (partial is-open grid)
+    (filterv (partial in-bounds grid)
+      (filterv #(not (includes? prev %))
         (adjacent-tiles at)))))
 
 (defn neighbors [grid at prev]
   (case (char-at grid at)
-    \^ (filter #(not= % prev) (list (list (- 1 (first at)) (second at))))
-    \< (filter #(not= % prev) (list (list (first at) (- 1 (second at)))))
-    \> (filter #(not= % prev) (list (list (first at) (+ 1 (second at)))))
-    \v (filter #(not= % prev) (list (list (+ 1 (first at)) (second at))))
+    \^ (filterv #(not (includes? prev %)) [[ (- 1 (first at)) (second at)]])
+    \< (filterv #(not (includes? prev %)) [[ (first at) (- 1 (second at))]])
+    \> (filterv #(not (includes? prev %)) [[ (first at) (+ 1 (second at))]])
+    \v (filterv #(not (includes? prev %)) [[ (+ 1 (first at)) (second at)]])
     (all-neighbors grid at prev)))
 
 (defn make-next [steps prev coord]
-  (list coord (+ 1 steps) prev))
+  [coord (+ 1 steps) prev])
 
 (defn make-nexts [grid state]
   (let [coord (first state)
         steps (second state)
         prev (nth state 2)
         n (neighbors grid coord prev)]
-    (map (partial make-next steps coord) n)))
+    (map (partial make-next steps (conj [coord] prev)) n)))
 
 (defn max-dist [grid dest best states]
   (if (empty? states) best
@@ -70,7 +66,7 @@
       (if (= coord dest)
         (recur grid dest (max best steps) (rest states))
         (recur grid dest best (concat (make-nexts grid state) (rest states)))))))
-
+ 
 (defn -main [& args]
   (let [input-file (first args)
         part (second args)
@@ -78,4 +74,4 @@
         grid (str/split content #"\n")
         start (start-pos grid)
         end (end-pos grid)]
-    (println (max-dist grid end 0 (list (list start 0 '((-1 -1))))))))
+    (println (max-dist grid end 0 [[start 0 [-1 -1]]]))))
