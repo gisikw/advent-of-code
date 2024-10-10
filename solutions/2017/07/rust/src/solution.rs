db563fe564193ce648e623ecfabdadd804529e45
Kevin Gisi <kevin.gisi@calendly.com>
2024-10-09 22:38:01 -0500

    Refactor/cleanup

--- solutions/2017/07/rust/src/solution.rs
+++ solutions/2017/07/rust/src/solution.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{HashMap,HashSet};
 
 #[derive(Clone, Debug)]
 struct Node<'a> {
@@ -20,30 +20,28 @@ fn main() {
 
     let nodes: Vec<Node> =
         content.lines().map(|line| {
-            match line.split(" -> ").collect::<Vec<&str>>()[..] {
-                [id_weight, children] => { 
-                    let vals: Vec<&str> = id_weight.split(" (").collect();
-                    let name = vals[0];
-                    let weight: i32 = vals[1][0..vals[1].len()-1].parse().unwrap();
-                    Some(Node { name, weight, total_weight: 0, children: children.split(", ").collect() })
-                }
-                [id_weight] => { 
-                    let vals: Vec<&str> = id_weight.split(" (").collect();
-                    let name = vals[0];
-                    let weight: i32 = vals[1][0..vals[1].len()-1].parse().unwrap();
-                    Some(Node { name, weight, total_weight: 0, children: vec![] })
-                },
-                _ => None
-            }.unwrap()
+            let parts: Vec<&str> = line.split(" -> ").collect();
+            let name_weight: Vec<&str> = parts[0].split(" (").collect();
+            let name = name_weight[0];
+            let weight: i32 = name_weight[1][0..name_weight[1].len()-1].parse().unwrap();
+            let children = if parts.len() > 1 {
+                parts[1].split(", ").collect()
+            } else {
+                vec![]
+            };
+            Node { name, weight, total_weight: 0, children }
         }).collect();
+
+    let all_children: HashSet<&str> = nodes
+        .iter()
+        .flat_map(|node| node.children.iter())
+        .cloned()
+        .collect();
 
     let root = nodes
         .iter()
-        .find(|node| {
-            !nodes.iter().any(|other| {
-              other.children.iter().any(|n| *n == node.name)
-            })
-        }).unwrap();
+        .find(|node| !all_children.contains(node.name))
+        .unwrap();
 
     if part == "1" {
         println!("{}", root.name);
@@ -54,8 +52,7 @@ fn main() {
     nodes.iter().for_each(|node| { map.insert(node.name, node.clone()); });
 
     let mut stack = vec![(root.name,false)];
-    while !stack.is_empty() {
-        let (name, visited) = stack.pop().unwrap();
+    while let Some((name, visited)) = stack.pop() {
         if visited {
             let node = map.get(name).unwrap();
             let total_weight: i32 = node.weight + node.children.iter().map(|c| map.get(c).unwrap().total_weight).sum::<i32>();
@@ -67,37 +64,27 @@ fn main() {
     }
 
     let mut stack = vec![(root.name,0)];
-    while !stack.is_empty() {
-        let (name, target_weight) = stack.pop().unwrap();
+    while let Some((name, target_weight)) = stack.pop() {
         let node = map.get(name).unwrap();
-        let children: Vec<&Node> = node.children.iter().map(|c| map.get(c).unwrap()).collect();
-        let weights: Vec<i32> = children.iter().map(|c| c.total_weight).collect();
-        if children.len() < 2 {
+        let children = node.children.iter().map(|c| map.get(c).unwrap());
+        let weights: Vec<i32> = children.clone().map(|c| c.total_weight).collect();
+        let mut uniq_weights: Vec<i32> = weights.iter().copied().collect();
+        uniq_weights.sort();
+        uniq_weights.dedup();
+        if children.len() < 2 || uniq_weights.len() == 1 {
             println!("{:?}", target_weight - weights.iter().sum::<i32>());
-        } else {
-            let mut uniq_weights = weights.clone();
-            uniq_weights.sort();
-            uniq_weights.dedup();
-            match uniq_weights.len() {
-                1 => {
-                    println!("{:?}", target_weight - weights.iter().sum::<i32>());
-                }
-                _ => {
-                    let correct_weight = match weights.len() {
-                        2 => {
-                            (target_weight - node.weight) / 2
-                        }
-                        _ => {
-                            if weights[0] == weights[1] || weights[0] == weights[2] {
-                                weights[0]
-                            } else {
-                                weights[1]
-                            }
-                        }
-                    };
-                    stack.push((children.iter().find(|c| c.total_weight != correct_weight).unwrap().name, correct_weight));
-                }
-            }
+            std::process::exit(0);
         }
+        let correct_weight = if weights.len() == 2{
+            (target_weight - node.weight) / 2
+        } else {
+            if weights[0] == weights[1] || weights[0] == weights[2] {
+                weights[0]
+            } else {
+                weights[1]
+            }
+        };
+        let offender = children.clone().find(|c| c.total_weight != correct_weight).unwrap().name;
+        stack.push((offender, correct_weight));
     }
 }
