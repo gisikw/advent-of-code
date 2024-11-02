ebb84708b5c153d234ffcaf1608e5a5e6001e3f6
Kevin Gisi <kevin.gisi@calendly.com>
2024-11-01 21:56:58 -0500

    Add yasl and miniscript support

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -26,7 +26,7 @@ pub fn run(year: usize, day: usize, language: &Option<String>, yes: &bool) {
     utils::copy_dir_all(&Path::new(&template_path), &Path::new(&target_path)).expect("Failed to copy template to solution directory");
 
     let fetch_message = "Solution created. Would you like to fetch the input?";
-    if utils::confirm(&fetch_message) && !yes {
+    if utils::confirm(&fetch_message) || (*yes && resolved_year != 9999) {
         commands::fetch::run(&Some(resolved_year), &Some(resolved_day));
     }
 }
