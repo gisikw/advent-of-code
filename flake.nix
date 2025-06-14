ec889d56d623cd60501b3c6091716e6faa1adedd
Kevin Gisi <kevin.gisi@calendly.com>
2025-06-14 00:10:56 -0500

    Migrate 14 languages over to nix

--- flake.nix
+++ flake.nix
@@ -12,8 +12,22 @@
         pkgs = import nixpkgs { inherit system; };
       in {
         devShells = {
-          ruby = pkgs.mkShell { packages = [ pkgs.ruby ]; };
+          bash = pkgs.mkShell { packages = [ pkgs.bash ]; };
+          c = pkgs.mkShell { packages = [ pkgs.gcc ]; };
           clojure = pkgs.mkShell { packages = [ pkgs.clojure pkgs.openjdk ]; };
+          crystal = pkgs.mkShell { packages = [ pkgs.crystal ]; };
+          dart = pkgs.mkShell { packages = [ pkgs.dart ]; };
+          elixir = pkgs.mkShell { packages = [ pkgs.elixir ]; };
+          erlang = pkgs.mkShell { packages = [ pkgs.erlang ]; };
+          go = pkgs.mkShell { packages = [ pkgs.go ]; };
+          kotlin = pkgs.mkShell { packages = [ pkgs.kotlin ]; };
+          lua = pkgs.mkShell { packages = [ pkgs.lua ]; };
+          nim = pkgs.mkShell { packages = [ pkgs.nim ]; };
+          ocaml = pkgs.mkShell { packages = [ pkgs.ocaml ]; };
+          perl = pkgs.mkShell { packages = [ pkgs.perl ]; };
+          php = pkgs.mkShell { packages = [ pkgs.php ]; };
+          ruby = pkgs.mkShell { packages = [ pkgs.ruby ]; };
+          tcl = pkgs.mkShell { packages = [ pkgs.tcl ]; };
         };
       }
     );
